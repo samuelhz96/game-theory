f3e485793d711c501e8857df58bb186925e5c858
samuelhz96 <77164950+samuelhz96@users.noreply.github.com>
2021-04-06 20:33:57 +0200

    Update CDF GTPS4.R

--- CDF GTPS4.R
+++ CDF GTPS4.R
@@ -13,6 +13,20 @@ s_2 <- seq(7,100,by=0.01)
 F_s_2 <- CDF(s_2)
 plot(s_2,F_s_2)
 
+s_3 <- seq(-3,3,by=0.01)
+F_s_3 <- CDF(s_3)
+plot(s_3,F_s_3)
+
+
+s_4 <- seq(0,3,by=0.01)
+F_s_4 <- CDF(s_4)
+plot(s_4,F_s_4)
+
+
+s_5 <- seq(0.01,3,by=0.01)
+F_s_5 <- CDF(s_5)
+plot(s_5,F_s_5)
+
 
 
 
